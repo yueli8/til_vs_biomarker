18d4bdfcc6310b0e493badf0f0d126c1a8f1a1ca
yueli8 <40526984+yueli8@users.noreply.github.com>
2022-01-30 15:06:38 +0800

    Add files via upload

---
+++ fig1_gse184053/gene_ontology_Enrichment.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+setwd("~/gse184053/data/data_finally")
+a<-read.table("Enrichment_plot.txt", head=TRUE, sep="\t")
+S1<- ggplot(a, aes(x= fold_Enrichment, y=reorder(GO, fold_Enrichment), size=counts,fill=FDR)) + geom_point(shape = 21) +theme_bw() +theme()
+S1=S1+ scale_fill_continuous(low = '#d90424', high = '#374a89')+scale_x_continuous(
+  labels = scales::number_format(accuracy = 0.1))+ theme(axis.text.y = element_text(size = 16, family = "Arial", color = "black", face = "bold"))+xlab("fold_Enrichment")+scale_x_continuous(breaks = seq(0, 45, by = 10))
+S1
