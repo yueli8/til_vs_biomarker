c736a8c70b3c0606dc83965ca653e863b5d9a1d0
yueli8 <40526984+yueli8@users.noreply.github.com>
2022-02-05 20:36:09 -0800

    Add files via upload

---
+++ fig6_gse123902/pheatmap.R
@@ -0,0 +1,11 @@
+library(pheatmap)
+setwd("~/geo/gse123902")
+#pheatmap
+mat<-read.table("tmp02", head=TRUE, row.names = 1, sep="\t", quote="")
+x<-as.matrix(mat)
+#x<-t(x)
+pheatmap(log((x+1),2),cellwidth=25, cellheight=20, cluster_cols=T,cluster_rows = T, 
+        color=colorRampPalette(c("green", "black", "red"))(100),fontsize=15)
+
+
+
